54a0692db59c8ff76fd8bfbcd33eca4db9908721
Shiva Bhusal <srollins175@gmail.com>
2026-03-15 21:38:29 +0545

    NAvigation Project Setup

---
+++ my_nav/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyNav());
+}
+
+class MyNav extends StatelessWidget {
+  const MyNav({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class name extends StatefulWidget {
+  const name({super.key});
+
+  @override
+  State<name> createState() => _nameState();
+}
+
+class _nameState extends State<name> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
