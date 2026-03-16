daf283d099e76fb6c7d88308265ce970b89d6a76
Shiva Bhusal <srollins175@gmail.com>
2026-03-16 13:02:00 +0545

    Database Create Init

---
+++ notes_app/lib/database/notes_database.dart
@@ -0,0 +1,54 @@
+import 'package:sqflite/sqflite.dart' as sqflite;
+import 'package:path/path.dart';
+
+class NotesDatabase {
+  static final NotesDatabase instance = NotesDatabase._init();
+
+  NotesDatabase._init();
+
+  static sqflite.Database? _database;
+
+  Future<sqflite.Database> get database async {
+    if (_database != null) return _database!;
+
+    _database = await _initDB('notes.db');
+    return _database!;
+  }
+
+  Future<sqflite.Database> _initDB(String filepath) async {
+    final dbPath = await sqflite.getDatabasesPath();
+    final path = join(dbPath, filepath);
+
+    return await sqflite.openDatabase(path, version: 1, onCreate: _createDb);
+  }
+
+  Future _createDb(sqflite.Database db, int version) async {
+    await db.execute('''
+
+    CREATE TABLE note(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      title TEXT NOT NULL,
+      description TEXT NOT NULL,
+      date TEXT NOT NULL,
+      color INTEGER NOT NULL DEFAULT 0
+    )
+
+ ''');
+  }
+
+  Future<int> addNotes(
+    String title,
+    String description,
+    String date,
+    int color,
+  ) async {
+    final db = await instance.database;
+    Map<String, dynamic> values = {
+      "title": title,
+      "description": description,
+      "date": date,
+      "color": color,
+    };
+    return await db.insert("note", values);
+  }
+}
