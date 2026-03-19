0c0756406567bf2c344f889adafe5f94786a803f
Shiva Bhusal <srollins175@gmail.com>
2026-03-19 11:30:47 +0545

    Home Screen Banner add

--- e_commerce/lib/Views/app_home_screen.dart
+++ e_commerce/lib/Views/app_home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/Widgets/banner.dart';
 import 'package:flutter/material.dart';
 
 class AppHomeScreen extends StatefulWidget {
@@ -15,16 +16,45 @@ class _AppHomeScreenState extends State<AppHomeScreen> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            const SizedBox( height: 50,),
+            const SizedBox(height: 60),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   // Image(image: "assets/images/logo.png");
-                  Image.asset("assets/images/logo.png", height: 40,)
+                  Image.asset("assets/images/logo.png", height: 50),
+                  Stack(
+                    clipBehavior: Clip.none,
+                    children: [
+                      Icon(Icons.shopping_bag, size: 30),
+                      Positioned(
+                        top: -5,
+                        right: -5,
+                        child: Container(
+                          padding: EdgeInsets.all(4),
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Center(
+                            child: Text(
+                              "3",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
+            const SizedBox(height: 20),
+            MyBanner()
           ],
         ),
       ),
