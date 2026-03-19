8aa39a17e7f2ada22ff224fda329de573d111ec5
Shiva Bhusal <srollins175@gmail.com>
2026-03-19 11:29:08 +0545

    Banner Widget

---
+++ e_commerce/lib/Widgets/banner.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:e_commerce/Utils/colors.dart';
+
+class MyBanner extends StatelessWidget {
+  const MyBanner({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      height: size.height * 0.23,
+      width: size.width,
+      color: bannerColor,
+      child: Padding(
+        padding: EdgeInsets.only(left: 27),
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "NEW COLLECTIONS",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: -2,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "20",
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: -3,
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "%",
+                          style: TextStyle(fontWeight: FontWeight.w900),
+                        ),
+                        Text(
+                          "OFF",
+                          style: TextStyle(
+                            fontSize: 10,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: -1.5,
+                            height: 0.6,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                MaterialButton(
+                  onPressed: () {},
+                  color: Colors.black,
+                  child: const Text(
+                    "SHOP NOW",
+                    style: TextStyle(fontSize: 12, color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+            Align(alignment: Alignment.bottomRight, child: Image.asset("assets/girl.png", height: size.height*0.19,), )
+          ],
+        ),
+      ),
+    );
+  }
+}
