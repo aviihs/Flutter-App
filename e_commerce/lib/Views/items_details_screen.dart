da199ed0c939a413ffff4d98bdfa705c0592de11
Shiva Bhusal <srollins175@gmail.com>
2026-03-22 11:02:01 +0545

    Items Details Page

--- e_commerce/lib/Views/items_details_screen.dart
+++ e_commerce/lib/Views/items_details_screen.dart
@@ -12,15 +12,59 @@ class ItemsDetailsScreen extends StatefulWidget {
 }
 
 class ItemsDetailsScreenState extends State<ItemsDetailsScreen> {
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: fbackgroundColor2,
         title: Text("Details Product"),
-        actions: [Header(showLogo: false),
+        actions: [Header(showLogo: false)],
+      ),
+      body: ListView(
+        children: [
+          Container(
+            color: fbackgroundColor2,
+            width: size.width,
+            height: size.height * 0.46,
+            child: PageView.builder(
+              onPageChanged: (value) {},
+              itemCount: 3,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    Image.asset(
+                      widget.eCommerceApp.image,
+                      height: size.height * 0.4,
+                      width: size.width * 0.85,
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(
+                        3,
+                        (index) => AnimatedContainer(
+                          duration: Duration(microseconds: 300),
+                          margin: EdgeInsets.only(right: 8),
+                          width: 7,
+                          height: 7,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: index == currentIndex ? Colors.blue : Colors.grey.shade400
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
